fa42469f41a95d3506855ca166908a220b43f111
Charlotte Koch <dressupgeekout@gmail.com>
2023-03-02 20:18:00 -0800

    WIP PDF and JPEG checker

---
+++ bin/checklist.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -ex
+
+
+## B&W TEXT REQUIREMENTS
+
+# Confirm resolution is 300 ppi (8.)
+
+# Confirm color space is Grayscale (8.)
+
+# Confirm LPI (lines per inch) is 175 visual (8.)
+
+# Confirm appropriate bleed (8.)
+
+# Confirm that all fonts in the PDF are embedded (9.)
+
+# Confirm page count is an even number (9.)
+
+# Confirm there no Spot colors or ICC profiles (9.)
+
+
+## COLOR COVER REQUIREMENTS
+
+# Confirm resolution is 300 ppi (18.)
+
+# Confirm color space is CMYK (18.)
+
+# Confirm LPI (lines per inch) is 180 (18.)
+
+# Confirm file name is of the appropriate pattern (18.)
+
+
+
+
+##### EBOOK REQUIREMENTS
+
+# Confirm eBook cover is JPEG (6.)
+
+# Confirm eBook cover is at least 1700 pixels tall (6.)
+
+# Confirm eBook cover uses RGB color space (6.)
